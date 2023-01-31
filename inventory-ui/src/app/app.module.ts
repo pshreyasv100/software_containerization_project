a12a3a7601572058a6dd15eb83a8527beffa5e01
Don Mani <donmarch14@gmail.com>
2023-01-31 18:21:34 +0100

    App components added

---
+++ inventory-ui/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MovieListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
